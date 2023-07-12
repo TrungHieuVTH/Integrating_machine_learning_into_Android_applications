d8720e719cc941bcf9d8588d0927c877d378ce59
Trieu Vu <vutrieu2002@gmail.com>
2023-07-12 16:08:32 +0700

    add a.txt

--- FinalProject_Nhom_3/app/src/main/java/com/example/FinalProject_Nhom_3/face/interfaces/FaceDetectStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.FinalProject_Nhom_3.face.interfaces;
-
-import com.example.FinalProject_Nhom_3.face.models.RectModel;
-
-public interface FaceDetectStatus {
-    void onFaceLocated(RectModel rectModel);
-    void onFaceNotLocated();
-}
